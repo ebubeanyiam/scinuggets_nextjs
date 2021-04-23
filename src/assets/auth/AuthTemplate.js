916b72e7c76f97eef8905c990ca32940d4e65b7f
Ebube <dbupanyiam@gmail.com>
2021-04-23 18:21:46 +0100

    Fixed bugs

--- src/assets/auth/AuthTemplate.js
+++ src/assets/auth/AuthTemplate.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Link from "next/Link";
+import Link from "next/link";
 
 import { authFunction } from "./AuthProvider";
 import { authOptions } from "./AuthOptions";
